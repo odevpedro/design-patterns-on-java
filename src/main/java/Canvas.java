c1cdd28c90b76f365a576d9d7badcd2cf30c3ceb
odevpedro <pedrosschmidt2@gmail.com>
2023-01-14 17:12:26 -0300

    the problem

---
+++ src/main/java/Canvas.java
@@ -0,0 +1,32 @@
+public class Canvas {
+    private ToolType currentTool;
+
+
+
+    public void mouseDown() {
+        if (currentTool == ToolType.SELECTION)
+            System.out.println("Selection icon");
+            else if (currentTool == ToolType.BRUSH)
+            System.out.println("Bruch icon");
+            else if (currentTool == ToolType.ERASER)
+            System.out.println("Eraser icon");
+    }
+
+    public void mouseUp(){
+        if (currentTool == ToolType.SELECTION)
+            System.out.println("Draw dashed rectangle");
+        else if (currentTool == ToolType.BRUSH)
+            System.out.println("Draw a line");
+        else if (currentTool == ToolType.ERASER);
+            System.out.println("Erase Something");
+    }
+
+
+    public ToolType getCurrentTool() {
+        return currentTool;
+    }
+
+    public void setCurrentTool(ToolType currentTool) {
+        this.currentTool = currentTool;
+    }
+}
