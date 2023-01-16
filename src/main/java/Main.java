86c6f433de0285f01a453620c61a57b9094edc7e
odevpedro <pedrosschmidt2@gmail.com>
2023-01-16 19:02:50 -0300

    template-method theory

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
+import template.TransferMoneyTask;
 
 public class Main {
     public static void main(String[] args) {
-
+    var task = new TransferMoneyTask();
+    task.execute();
     }
      }
 
---
+++ src/main/java/template/Task.java
@@ -0,0 +1,18 @@
+package template;
+
+public abstract class Task {
+    private AuditTrail auditTrail;
+
+    public Task() {}
+
+    public Task(AuditTrail auditTrail) {
+        this.auditTrail = auditTrail;
+    }
+
+        public void execute(){
+        auditTrail.record();
+        doExecute();
+    }
+
+    protected abstract void doExecute();
+}
